befef8b1a1f28a885edb53d8551ade5cb243182e
AmazadKhan <136595707+AmazadKhan@users.noreply.github.com>
2024-08-05 07:39:47 -0700

    javascript project

---
+++ calc.html/calculate.js
@@ -0,0 +1,6 @@
+// This function evaluates the expression and returns the result
+function calculate() {
+    // var p = document.getElementById("result").value;
+    var q = eval(p);
+    // document.getElementById("result").value=q;
+}
